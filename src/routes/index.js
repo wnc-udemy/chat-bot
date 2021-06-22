1e92b6b43dc01d331fe60fe42c615e8fc938201a
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2021-06-22 19:49:10 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 // import homepageController from '../controllers/homepageController';
-import chatBotController from '../controllers/chatBotController';
+import chatBotController from '../controllers';
 
 let router = express.Router();
 
